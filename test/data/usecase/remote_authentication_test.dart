8407314e2c06b1b90416f32ac72f606157c6dbcb
Renato Junior <49106675+Renatojr2@users.noreply.github.com>
2020-11-29 17:24:48 -0300

    refactory test params

--- test/data/usecase/remote_authentication_test.dart
+++ test/data/usecase/remote_authentication_test.dart
@@ -1,6 +1,7 @@
 import 'package:faker/faker.dart';
 import 'package:fordev/data/http/http.dart';
 import 'package:fordev/data/usecases/usecases.dart';
+import 'package:fordev/domain/helpes/helpes.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -11,18 +12,19 @@ class HttpClientSpy extends Mock implements HttpClient {}
 void main() {
   RemoteAuthentication sut;
   HttpClientSpy httpClient;
+  AuthenticationParams params;
   String url;
 
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
-  });
-  test('Should call HttpClient with corrent values', () async {
-    final params = AuthenticationParams(
+    params = AuthenticationParams(
       email: faker.internet.email(),
       password: faker.internet.password(),
     );
+  });
+  test('Should call HttpClient with corrent values', () async {
     sut.auth(params);
 
     verify(httpClient.request(
@@ -30,5 +32,16 @@ void main() {
       method: 'post',
       body: {'email': params.email, 'password': params.password},
     ));
+  });
+  test('Should throw unexpectedError if HttpClient return 400', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
