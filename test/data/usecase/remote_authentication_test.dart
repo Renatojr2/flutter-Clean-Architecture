91f69c51345de80fd17dd2929a24f2e8e72c8fd3
Renato Junior <49106675+Renatojr2@users.noreply.github.com>
2020-11-29 17:32:45 -0300

    test erro invalid credentials 401

--- test/data/usecase/remote_authentication_test.dart
+++ test/data/usecase/remote_authentication_test.dart
@@ -55,4 +55,27 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+  test('Should throw unexpectedError if HttpClient return 500', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+  test('Should throw InvalidCredentialsError if HttpClient return 401',
+      () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.unauThorized);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.invalidCredentials));
+  });
 }
