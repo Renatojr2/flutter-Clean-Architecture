a80a2b12fb64c526660c07e2aa943dd22146c0d5
Renato Junior <49106675+Renatojr2@users.noreply.github.com>
2020-12-05 18:36:40 -0300

    ensure HttpAdapter calls without body

--- test/infra/http/http_adpter_test.dart
+++ test/infra/http/http_adpter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -9,15 +11,14 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({
-    @required String url,
-    @required String method,
-  }) async {
+  Future<void> request(
+      {@required String url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'aplication/json',
       'accept': 'aplication/json'
     };
-    client.post(url, headers: headers);
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -35,12 +36,28 @@ void main() {
   });
   group('post', () {
     test('Should call post with correct value', () async {
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+
+      verify(
+        client.post(url,
+            headers: {
+              'content-type': 'aplication/json',
+              'accept': 'aplication/json'
+            },
+            body: '{"any_key":"any_value"}'),
+      );
+    });
+
+    test('Should call post without body', () async {
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url, headers: {
-        'content-type': 'aplication/json',
-        'accept': 'aplication/json'
-      }));
+      verify(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
+        ),
+      );
     });
   });
 }
