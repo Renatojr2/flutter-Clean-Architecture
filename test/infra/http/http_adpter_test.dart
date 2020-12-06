d11263c96f01dea9c70687160a392138e23c5cef
renato junior <momentum323@gmail.com>
2020-12-06 14:55:52 -0300

    move duplicated code to a priveted method

--- test/infra/http/http_adpter_test.dart
+++ test/infra/http/http_adpter_test.dart
@@ -1,34 +1,10 @@
 import 'dart:convert';
 
 import 'package:faker/faker.dart';
+import 'package:fordev/infra/http/http.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-
-import '../../../lib/data/http/http.dart';
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request(
-      {@required String url, @required String method, Map body}) async {
-    final headers = {
-      'content-type': 'aplication/json',
-      'accept': 'aplication/json'
-    };
-
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(url, headers: headers, body: jsonBody);
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else {
-      return null;
-    }
-  }
-}
 
 class ClientSpy extends Mock implements Client {}
 
