0e14d3f3df2f5e69aa0f5483c79d2debb118548e
renato junior <momentum323@gmail.com>
2020-12-06 16:10:37 -0300

    error 500

--- test/infra/http/http_adpter_test.dart
+++ test/infra/http/http_adpter_test.dart
@@ -35,6 +35,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -125,6 +129,13 @@ void main() {
     });
     test('Should return ServerError if returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+    test('Should return ServerError if post throw', () async {
+      mockError();
 
       final future = sut.request(url: url, method: 'post');
 
